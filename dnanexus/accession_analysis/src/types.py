a2e5358f6ee1f20ea446ab458ba874c6059bf775
Ulugbek Baymuradov <ulugbekbk@gmail.com>
2018-03-13 18:20:39 -0700

    Stage class definition

--- dnanexus/accession_analysis/src/types.py
+++ dnanexus/accession_analysis/src/types.py
@@ -97,6 +97,7 @@ class File(EncodedObject):
     def __init__(self, data=None, **kwargs):
         super().__init__(data, kwargs)
 
+
     def post(self):
         self.make_post_request()
 
@@ -142,20 +143,13 @@ class Experiment(EncodedObject):
     def fastq_replicates_by_number(self, number):
         return [file for file in self.fastqs if 
                     file.biological_replicate_number == number]
-
-
-class Stages(object):
-    """docstring for Stages"""
-    def __init__(self, arg):
-        super(Stages, self).__init__()
-        self.arg = arg
         
 
 
 class Analysis(object):
     """docstring for Analysis"""
     def __init__(self, analysis_id):
-        super(Analysis, self).__init__()
+        super().__init__()
         self.analysis_id = analysis_id
         self.analysis = dxpy.describe(analysis_id)
         self.experiment_accession = self.get_experiment_accession()
@@ -163,6 +157,7 @@ class Analysis(object):
             self.experiment = Experiment(accession=self.experiment_accession)
         except AttributeError:
             continue
+        self.
 
 
     def get_experiment_accession(self):
@@ -193,7 +188,7 @@ class Analysis(object):
             return experiment_accession
     
     @property
-    def is_unreplicated_analysis(self):
+    def is_unreplicated(self):
         return (
             self.analysis['properties'].get('unreplicated_experiment') in ['True', 'true']
             or self.analysis['properties'].get('simplicate_experiment') in ['True', 'true'])
@@ -202,9 +197,99 @@ class Analysis(object):
     def is_unary_control(self):
         return analysis['properties'].get('unary_control') in ['True', 'true']
 
+class OutputFile(object):
+    """docstring for OutputFile"""
+    def __init__(self, arg):
+        super().__init__()
+        self.arg = arg
+        
+
+class Stages(object):
+    """docstring for Stages"""
+    def __init__(self, analysis, **kwargs):
+        self.analysis = analysis
+        self.fqchek = kwargs.get('fqchek', False)
+        
+    @cached_property
+    def stages(self):
+        return [stage['execution'] for stage in self.analysis.get('stages')]
+
+    def stage_by_name(self, stage_name):
+        peaks_stage = next(
+            stage for stage in self.stages
+            if stage['name'] == stage_name)
+
+    def mapping_stages_from_tas(self, tas):
+        mapping_jobs = \
+            [dxpy.describe(ta['createdBy']['job'])
+             for ta in tas]
+
+        mapping_analyses = \
+            [dxpy.describe(mapping_job['analysis'])
+             for mapping_job in mapping_jobs if mapping_job]
+
+        mapping_stages = []
+        for (i, repn) in enumerate(reps):
+            mapping_stage = \
+                get_mapping_stages(
+                    mapping_analyses[i], keypair, server, fqcheck, repn)
+            if not mapping_stage:
+                logger.error('%s: failed to find mapping stages for rep%d'
+                             % (peaks_analysis['id'], repn))
+                return None
+            else:
+                mapping_stages.append(mapping_stage)
+
+        return mapping_stages
 
 
+    @cached_property
+    def peak_mapping_stages(self):
+        # Find the tagaligns actually used as inputs into the analysis
+        # Find the mapping analyses that produced those tagaligns
+        # Find the filtered bams from those analyses
+        # Build the stage dict and return it
+        
+        peaks_analysis = self.analysis
+        logger.debug(
+            'in get_peak_mapping_stages: peaks_analysis is {} named {}'.format(
+                peaks_analysis.get('id'), peaks_analysis.get('name')))
+
+        if peaks_analysis.is_unreplicated:
+            reps = [1]
+        else:
+            reps = [1, 2]
 
 
+        peaks_stage = self.stage_by_name("ENCODE Peaks")
+
+        tas = [dxpy.describe(peaks_stage['input']['rep%s_ta' % (n)])
+               for n in reps]
+
+        return mapping_stages_from_tas(tas)
 
 
+    @cached_property
+    def control_mapping_stages(self):
+        # Find the control inputs
+
+        peaks_analysis = self.analysis
+        logger.debug(
+            'in get_control_mapping_stages with peaks_analysis %s'
+            % (peaks_analysis['id']))
+
+        peaks_stage = self.stage_by_name("ENCODE Peaks")
+
+        # here reps is always 1,2 because the peaks job rep numbers are 1,2 ...
+        # these are not the ENCODEd biological_replicate_numbers, which are
+        # only known to the analysis via its name, or by going back to ENCODEd and
+        # figuring out where the fastqs came from
+        reps = sorted([
+            re.match("ctl(\d+)_ta", input_key).group(1)
+            for input_key in peaks_stage['input'].keys()
+            if re.match("ctl(\d+)_ta", input_key)])
+
+        tas = [dxpy.describe(peaks_stage['input']['ctl%s_ta' % (n)])
+               for n in reps]
+
+        return mapping_stages_from_tas(tas)
