2ae8430a548d5a46cedbf18754c1e795996f56e6
J. Seth Strattan <jseth@stanford.edu>
2018-04-30 14:01:54 -0700

    Changes required for Ubuntu 16

--- dnanexus/shell/src/shell.sh
+++ dnanexus/shell/src/shell.sh
@@ -4,17 +4,6 @@ main() {
 
     echo "Hours to live: '$hours_to_live'"
 
-    sudo mv /etc/apt/apt.conf.d/99dnanexus /tmp
-    sudo add-apt-repository -y ppa:deadsnakes/ppa
-    sudo apt-get update
-    sudo apt-get -y install python3.4-dev libfreetype6-dev
-
-    source env3/bin/activate
-    mkdir idr_test
-    cd idr_test
-    idr --version
-    idr --samples ~/env3/idr/tests/data/peak1 ~/env3/idr/tests/data/peak2 --plot --output-file IDR_test.narrowPeak --log-output-file IDR_test-out.txt
-
     sleep $(($hours_to_live * 3600))
 
 }
